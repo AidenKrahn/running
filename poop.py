adb1ff70be1f7bee634601e25c933a7274d57b3d
AidenKrahn <125599022+AidenKrahn@users.noreply.github.com>
2023-05-15 21:07:08 -0600

    Update poop.py

--- poop.py
+++ poop.py
@@ -1,4 +1,4 @@
-import pygame, random
+import pygame, random, time
 
 pygame.init()
 
@@ -26,10 +26,14 @@ class Human(object):
         self.color = color
         
     def hit(self,win):
+        bg = red
+        win.fill(bg)
+        pygame.time.delay(100)
         print('hit')
+        
     
 class Dodge(object):
-    def __init__(self,width,height):
+    def __init__(self,width,height,color):
         dio = random.randint(1,4)
         if dio == 1:
             self.x = random.randint(10,590)
@@ -54,18 +58,37 @@ class Dodge(object):
         self.width = width
         self.height = height
         self.vel = 5
+        self.color = color
         
     def draw(self,win):
-        pygame.draw.rect(win, black, (self.x, self.y, self.width, self.height))
+        pygame.draw.rect(win, self.color, (self.x, self.y, self.width, self.height))
 
+class Orb(Dodge):
+    def __init__(self,width,height,x,y):
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+        self.vis = True
+        self.color = blue
         
+    def start(self,win):
+        print('welcome')
+        self.vis = False
         
+    def froth(self,win):
+        super().draw(win)
+        
+bg = white
 good = True
 grodd = Human(50,50,50,50,black)
 loop = []
+cold = Orb(50,50,250,250)
 font1 = pygame.font.SysFont('timesnewroman', 20)
 text = font1.render('Press keys to do random things!', 1, (255, 255, 255))
 win.blit(text, (300 - (text.get_width()/2), 100))
+text1 = font1.render('(ignore blue thing)', 1, (255, 255, 255))
+win.blit(text1, (300 - (text.get_width()/2), 200))
 pygame.display.update()
 i = 0
 while i < 200:
@@ -78,12 +101,15 @@ while i < 200:
 
 while good == True:
     pygame.time.delay(30)
-    win.fill(white)
+    win.fill(bg)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             good = False
-            
+            break
+        
     pygame.draw.rect(win,grodd.color,(grodd.x,grodd.y,grodd.width,grodd.height))
+    if cold.vis == True:
+        cold.froth(win)
     for loops in loop:
         loops.draw(win)
         if loops.facing == 1:
@@ -114,12 +140,17 @@ while good == True:
             else:
                 loop.pop(loop.index(loops))
                 
-        if man.hitbox[1] < goblin.hitbox[1] + goblin.hitbox[3] and man.hitbox[1] + man.hitbox[3] > goblin.hitbox[1]:
+        if loops.x > grodd.x and loops.x < grodd.x + grodd.width:
+            if loops.y > grodd.y and loops.y < grodd.y + grodd.height:
+                grodd.hit(win)
+                loop.pop(loop.index(loops))
+    if cold.vis == True:
+        if cold.x + cold.width > grodd.x and cold.x < grodd.x + grodd.width:
+            if cold.y + cold.height > grodd.y and cold.y < grodd.y + grodd.height:
+                cold.start(win)
             
-            if man.hitbox[0] + man.hitbox[2] > goblin.hitbox[0] and man.hitbox[0] < goblin.hitbox[0] + goblin.hitbox[2]:
-                man.hit()
-    
     keys = pygame.key.get_pressed()
+            
     if keys[pygame.K_LEFT] and grodd.x > grodd.vel:
         grodd.x -= grodd.vel
         
@@ -180,7 +211,10 @@ while good == True:
         break
     
     if keys[pygame.K_v] and len(loop) < 5:
-        loop.append(Dodge(5,5))
+        loop.append(Dodge(5,5,black))
+        
+    if keys[pygame.K_j]:
+        bg = green
         
     
     pygame.display.update()
